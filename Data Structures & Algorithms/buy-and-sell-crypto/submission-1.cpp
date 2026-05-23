400e27225056f67cb0595f3982a438f745edd72d
Ajit Ashwath <141044380+ajitashwath@users.noreply.github.com>
2026-05-23 20:57:24 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size(), ans = 0, minn = prices[0];
+         for(int i = 0; i < n; i++) {
+            if(prices[i] > minn) ans = max(ans, prices[i] - minn);
+            else minn = prices[i];
+        }
+        return ans;
+    }
+};
