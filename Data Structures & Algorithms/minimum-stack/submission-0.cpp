523754cdeec246845144ece739f1b81199c4fddd
Ajit Ashwath <141044380+ajitashwath@users.noreply.github.com>
2026-05-25 00:13:15 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,25 @@
+class MinStack {
+public:
+    stack<int> stk;
+    stack<int> minstk;
+
+    MinStack() {}
+    
+    void push(int val) {
+        stk.push(val);
+        if(minstk.empty() || val <= minstk.top()) minstk.push(val);
+    }
+    
+    void pop() {
+        if(stk.top() == minstk.top()) minstk.pop();
+        stk.pop();
+    }
+    
+    int top() {
+        return stk.top();        
+    }
+    
+    int getMin() {
+        return minstk.top();
+    }
+};
